91ca7be306cb49c152888024490be32b0051b163
Isabelle <isasoarez21@gmail.com>
2025-03-04 22:53:15 -0300

    feat: structure Home component

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,7 +1,31 @@
+import style from './home.module.css';
+
+import {useNavigate, Link} from 'react-router-dom';
 
 const Home = () => {
+  const [query, setQuery] = useState('');
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {}
   return (
-    <div>home</div>
+    <div className={style.home}>
+      <h1>Veja os posts mais recentes</h1>
+      <form onClick={handleSubmit} className={style.searchform}>
+        <input type='text' placeholder='Busque por tags' onChange={(e) => setQuery(e.target.value)}/>
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        {posts && posts.length === 0 && (
+          <div className={style.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+        
+      </div>
+    </div>
   )
 }
 
