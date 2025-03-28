6659b52072f74834359403bc58050524f2d70bdf
Isabelle <isasoarez21@gmail.com>
2025-03-28 15:46:51 -0300

    feat: implement post listing and management in Dashboard component

--- src/pages/DashBoard/DashBoard.jsx
+++ src/pages/DashBoard/DashBoard.jsx
@@ -1,8 +1,53 @@
-import styles from './DashBoard.module.css' 
+import styles from './DashBoard.module.css';
+
+import {Link} from "react-router-dom";
+
+
+import {useAuthValue} from '../../context/AuthContext';
+import {useFetchDocuments} from '../../hooks/useFetchDocument';
 
 const DashBoard = () => {
+  const {user} = useAuthValue();
+  const uid = user.uid;
+
+  const {documents: posts, loading} = useFetchDocuments('posts', null, uid);
+
+  const deletePost = (id) => {}
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
   return (
-    <div>DashBoard</div>
+    <div className={styles.dashboard}>
+        <h2>Dashboard</h2>
+        <p>Gerencie seus posts</p>
+        {posts && posts.length === 0 ? (
+          <div className={styles.nopost}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">Criar primeiro post</Link>
+          </div>
+        ) : (
+          <>
+          <div className={styles.post_header}>
+            <span>Título</span>
+            <span>ações</span>
+          </div>
+            {posts &&posts.map((post) => <div key={post.id} className={styles.post_row}>
+              <p>{post.title}</p>
+              <div>
+                <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                  Visualizar
+                </Link>
+                <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">
+                  Editar
+                </Link>
+                <button onClick={() => deletePost(post.id)}>Deletar</button>
+              </div>
+            </div>)}
+          </>
+        )}
+        
+    </div>
   )
 }
 
